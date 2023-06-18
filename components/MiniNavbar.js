79e50326e56b22390acb5c3c7eb2acbc6a7f1719
vicktor349 <ayomidevictor349@gmail.com>
2023-06-18 20:54:12 +0100

    Added MiniNavbar

--- components/MiniNavbar.js
+++ components/MiniNavbar.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import shipping from '../public/image/logo/overseas-shipping.png'
+import Image from 'next/image'
+import Button from './Button'
 
 const MiniNavbar = () => {
     return (
-        <div className='bg-black text-white'>
-            <div>
-                Gig
+        <div className='ssm:hidden xl:flex bg-black text-white'>
+            <div className='flex items-center p-3 mx-64'>
+                <Image src={shipping} alt='Overseas Shipping Logo' className='h-12 max-w-min' />
+                <div className='px-6'>
+                    <p>GIGL China is LIVE!! Shop, Ship & Make ALL Payments in Naira</p>
+                </div>
+                <Button text='Learn More' />
             </div>
         </div>
     )
