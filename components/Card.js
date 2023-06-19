d3dc7622a9b38b68022bc586cf3ed560c6d25f11
vicktor349 <ayomidevictor349@gmail.com>
2023-06-19 22:13:11 +0100

    Centered Items in mobile size view

--- components/Card.js
+++ components/Card.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Card = ({ href, text, icon, info }) => {
     return (
-        <Link href={href} className='bg-white py-12 px-8 border-gray-300 border-[1px] border-solid hover:cursor-pointer hover:bg-black hover:text-white'>
-            <div className='text-6xl font-light'>{icon}</div>
+        <Link href={href} className='ssm:text-center bg-white py-12 px-8 border-gray-300 border-[1px] border-solid hover:cursor-pointer hover:bg-black hover:text-white'>
+            <div className='text-6xl font-light flex justify-center'>{icon}</div>
             <div className='hover:text-white'>
                 <p className='py-5 text-gray-600 font-semibold text-xl'>
                     {text}
