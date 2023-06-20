b1f6c05bd40a6ceba545646d4750452eaa5353cc
vicktor349 <ayomidevictor349@gmail.com>
2023-06-20 20:29:40 +0100

    custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
         'primary': 'rgba(213,52,58)',
         'secondary': 'rgba(55,65,81,0.9)',
         'color': 'rgba(55,65,81,0.1)',
-        'background': 'rgba(243,244,246)'
+        'background': 'rgba(243,244,246)',
+        'text': '#374151'
       },
       screens: {
         'ssm': '360px'
