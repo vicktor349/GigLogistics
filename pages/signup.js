f6665c98f0c59c3baf50b4674baeacbdac2bc956
vicktor349 <ayomidevictor349@gmail.com>
2023-06-22 01:04:52 +0100

    Added appropriate icon

--- pages/signup.js
+++ pages/signup.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import React from 'react'
 import { Input, PasswordInput } from '@mantine/core'
 import Link from 'next/link'
+import { CgProfile } from 'react-icons/cg'
+import { IoAtSharp } from 'react-icons/io5'
+import { RiLockPasswordLine } from 'react-icons/ri'
 
 
 const signup = () => {
@@ -17,12 +20,12 @@ const signup = () => {
                     </div>
                     <div>
                         <div className='grid grid-cols-2 gap-8'>
-                            <Input placeholder='First Name' className='mb-8' size='lg' />
-                            <Input placeholder='Last Name' className='mb-8' size='lg' />
+                            <Input icon={<CgProfile size='24' />} placeholder='First Name' className='mb-8' size='lg' />
+                            <Input icon={<CgProfile size='24' />} placeholder='Last Name' className='mb-8' size='lg' />
                         </div>
-                        <Input type='email' size='lg' className='mb-8' placeholder='Email Address' />
-                        <PasswordInput size='lg' className='mb-8' placeholder='Password' />
-                        <PasswordInput size='lg' className='mb-8' placeholder='Confirm Password' />
+                        <Input icon={<IoAtSharp size='24' />} type='email' size='lg' className='mb-8' placeholder='Email Address' />
+                        <PasswordInput icon={<RiLockPasswordLine size='24' />} size='lg' className='mb-8' placeholder='Password' />
+                        <PasswordInput icon={<RiLockPasswordLine size='24' />} size='lg' className='mb-8' placeholder='Confirm Password' />
                         <button className='mb-8 text-white bg-black font-bold w-full p-4 rounded-xl'>
                             Sign Up
                         </button>
